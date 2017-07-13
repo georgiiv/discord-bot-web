bef82b58ef410f132cc3ee2c81f5626ebafe8b01
Georgi Ivanov <georgi.iv.ivanov@abv.bg>
2017-07-13 03:01:19 +0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -9,9 +9,9 @@ const client = new Discord.Client();
 app.use(bodyParser.json());
 
 //******************************************************************************
-const token = 'MzE4NDA3OTU5MjI0OTA5ODI0.DA4AKg.TAcRaJeY3gn_MggGg_n1kaZ1lss';
+const token = '';
 const port = 8080;
-const hostlink = "http://localhost:8080";
+const hostlink = "";
 prefix = '!';
 //******************************************************************************
 
@@ -19,33 +19,33 @@ client.on('ready', () => {
 	console.log("Bot is connected");
 });
 
-function playSound(id, file){
+function playSound(id, file) {
 	let channel = client.channels.get(id);
 
 	channel.join().then(connection => {
 		const broadcast = client
-		.createVoiceBroadcast()
-		.playFile('./sounds/' + file);
+			.createVoiceBroadcast()
+			.playFile('./sounds/' + file);
 		const dispatcher = connection.playBroadcast(broadcast);
 	}).catch(console.error);
 }
 
-app.get('/', function(req, res){
-		res.sendFile(__dirname + "/index.html");
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + "/index.html");
 })
 
-app.get('/files', function(req, res){
+app.get('/files', function (req, res) {
 	fs.readdir("./sounds", (err, files) => {
 		res.send(JSON.stringify(files));
 	});
 })
 
-app.post('/play', function(req, res) {
+app.post('/play', function (req, res) {
 	playSound(req.body.id, req.body.file);
 	res.send("It works");
 })
 
-app.post('/upload', function(req, res) {
+app.post('/upload', function (req, res) {
 	var storage = multer.diskStorage({
 		destination: function (request, file, callback) {
 			callback(null, './sounds');
@@ -54,60 +54,62 @@ app.post('/upload', function(req, res) {
 			callback(null, file.originalname)
 		}
 	});
-	var upload = multer({storage: storage}).single('sound');
+	var upload = multer({ storage: storage }).single('sound');
 
-	upload(req, res, function(err) {
-		if(err){
+	upload(req, res, function (err) {
+		if (err) {
 			console.log(err);
 			return;
 		}
 		//console.log(req.file.originalname + " uploaded");
-		if(req.body.channelid){
-			res.redirect('/?id='+req.body.channelid);
-		}else{
+		if (req.body.channelid) {
+			res.redirect('/?id=' + req.body.channelid);
+		} else {
 			res.send("No voice channel ID sent")
 		}
 	})
 });
 
-
+//-----------------------------------------------------------------------------------------------
+//Chat commands start here
+//-----------------------------------------------------------------------------------------------
 
 client.on('message', message => {
-	if(message.content === (prefix + 'ping')){
+	if (message.content === (prefix + 'ping')) {
 		message.reply('pong');
 	}
 
-	if(message.content === (prefix + 'join')){
+	if (message.content === (prefix + 'join')) {
 		const channel = message.member.voiceChannel;
 
 		channel.join()
-		.then(connection => {})
-		.catch(console.error);
+			.then(connection => { })
+			.catch(console.error);
 
 		message.delete()
 	}
 
-	if(message.content === (prefix + 'leave')){
+	if (message.content === (prefix + 'leave')) {
 		const channel = message.member.voiceChannel;
 		channel.leave();
 		message.delete()
 	}
 
 
-	if(message.content.startsWith(prefix + 'sound')){
+	if (message.content.startsWith(prefix + 'sound')) {
 		const channel = message.member.voiceChannel;
 
 		sound = message.content.split(" ")[1];
 
-		playSound(channel.id, sound+".mp3");
+		playSound(channel.id, sound + ".mp3");
 		message.delete()
 	}
 
 
-	if(message.content.startsWith(prefix + 'list')){
+	if (message.content.startsWith(prefix + 'list')) {
 		option = message.content.split(" ")[1];
 
-		if(option=="sounds"){			
+		if (option == "sounds") {
 			const fs = require('fs');
 			result = "```\n";
 
@@ -115,19 +117,19 @@ client.on('message', message => {
 				files.forEach(file => {
 					result = result + file.split(".mp3")[0] + "\n";
 				});
-				message.reply(result+"```");
+				message.reply(result + "```");
 				message.delete()
 			})
 		}
 	}
 
-	if(message.content === (prefix + 'link')){
+	if (message.content === (prefix + 'link')) {
 		message.reply(hostlink + "/?id=" + message.member.voiceChannel.id);
 		message.delete()
 	}
 
-	if(message.content === (prefix + 'tts')){
-		message.channel.send("This is a tts message", {tts: true});
+	if (message.content === (prefix + 'tts')) {
+		message.channel.send("This is a tts message", { tts: true });
 		message.delete()
 	}
 });
